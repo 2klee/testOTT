5af305463c145f632e5593db90422e968f09ce5b
admin <terious.kr@gmail.com>
2024-02-22 15:24:43 +0900

    영화 ui 수정

--- app/src/main/java/com/example/testott/MainActivity.kt
+++ app/src/main/java/com/example/testott/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+
         setSupportActionBar(binding.toolbar)
 
         val navView: BottomNavigationView = binding.navView
